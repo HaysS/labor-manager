b8a99532ae089112a75ecef82a0fedc58ef76229
Hays <stanfordhays@gmail.com>
2016-11-12 15:47:08 -0600

    Added basic navigation

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -5,8 +5,11 @@ class App extends Component {
 	render() {
 		return (
 			<Router history={hashHistory}>
-				<Route path='/' component={Home} />
-				<Route path='/page' component={Page} />
+				<Route path='/' component={Container}>
+					<IndexRoute component={Home} />
+					<Route path='/page' component={Page} />
+					<Route path='*' component={NotFound} />
+				</Route>
 			</Router>
 		);
 	}
@@ -14,5 +17,13 @@ class App extends Component {
 
 const Home = () => <h1>Hello from Home!</h1>;
 const Page = () => <h1>This is a blank page.</h1>;
+const NotFound = () => <h1>404 Error: Page not found.</h1>;
+const Nav = () => (
+	<div>
+		<Link to='/'>Home</Link>&nbsp;
+		<Link to='/page'>Page</Link>
+	</div>
+);
+const Container = (props) => <div><Nav />{props.children}</div>;
 
 export default App;
