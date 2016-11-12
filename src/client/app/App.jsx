8656bc877c99cd59322b12e109088652989b41b1
Hays <stanfordhays@gmail.com>
2016-11-12 16:22:17 -0600

    Implemented a css reset and added more basic styling to all pages

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -15,15 +15,20 @@ class App extends Component {
 	}
 }
 
-const Home = () => <h1>Hello from Home!</h1>;
-const Page = () => <h1>This is a blank page.</h1>;
-const NotFound = () => <h1>404 Error: Page not found.</h1>;
+const Home = () => <div className="left-col column"><h1>Hello from Home!</h1></div>;
+const Page = () => <div className="left-col column"><h1>This is a blank page.</h1></div>;
+const NotFound = () => <div className="left-col column"><h1>404 Error: Page not found.</h1></div>;
 const Nav = () => (
-	<div>
-		<Link to='/'>Home</Link>&nbsp;
-		<Link to='/page'>Page</Link>
-	</div>
+	<header>
+		<div className="header-content">
+			<h1 className="left-col column">Dating App</h1>
+			<nav className="right-col column">
+				<Link to='/'>Home</Link><p> | </p>
+				<Link to='/page'>Page</Link>
+			</nav>
+		</div>
+	</header>
 );
-const Container = (props) => <div><Nav />{props.children}</div>;
+const Container = (props) => <div className="wrapper"><Nav /><div className="main-content">{props.children}</div></div>;
 
 export default App;
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 
+require('./assets/reset.css');
 require('./assets/styles.css');
 
 ReactDOM.render(<App />, document.getElementById('container'));
