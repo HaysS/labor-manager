1464e55f5b4d5ac1f54804623df349a1c765e8f7
Hays <stanfordhays@gmail.com>
2016-11-12 16:25:43 -0600

    main content is no longer sepereated into seperate columns

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -15,9 +15,9 @@ class App extends Component {
 	}
 }
 
-const Home = () => <div className="left-col column"><h1>Hello from Home!</h1></div>;
-const Page = () => <div className="left-col column"><h1>This is a blank page.</h1></div>;
-const NotFound = () => <div className="left-col column"><h1>404 Error: Page not found.</h1></div>;
+const Home = () => <div className="column"><h1>Hello from Home!</h1></div>;
+const Page = () => <div className="column"><h1>This is a blank page.</h1></div>;
+const NotFound = () => <div className="column"><h1>404 Error: Page not found.</h1></div>;
 const Nav = () => (
 	<header>
 		<div className="header-content">
