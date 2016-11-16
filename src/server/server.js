1a133a3b217349aa2133e1b5d126e4eeb539c6b7
Hays <stanfordhays@gmail.com>
2016-11-15 22:36:03 -0600

    Firebase admin is now initialized in server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
+var admin = require('firebase-admin');
 
 const port = process.env.PORT || 8080;
 
+// Initialize Firebase
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://dating-app-954e8.firebaseio.com"
+});
+
+//Initialize Express App
 const app = express();
 app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.listen(port, function() {
 	console.log("server started on port "+port);
 });
+
+
