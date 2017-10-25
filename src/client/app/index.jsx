1fd770e3b46ffc5cb32b4cdaca43039922168c43
Hays <stanfordhays@gmail.com>
2017-10-25 10:30:59 -0500

    Changed app.

--- src/client/app/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
-
-require('./assets/reset.css');
-require('./assets/styles.css');
-
-ReactDOM.render(<App />, document.getElementById('container'));
